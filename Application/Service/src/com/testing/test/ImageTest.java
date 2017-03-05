ae224f24a8d7ef5ed44d01ca746bd787ebf6e15c
snaveenandrew <snaveenandrew@gmail.com>
2017-03-05 20:31:02 +0530

    Service

---
+++ Application/Service/src/com/testing/test/ImageTest.java
@@ -0,0 +1,13 @@
+package com.testing.test;
+
+public class ImageTest {
+
+	public ImageTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
