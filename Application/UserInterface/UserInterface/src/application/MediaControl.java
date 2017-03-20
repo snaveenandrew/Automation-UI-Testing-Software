e148f49144d2048b071247c3ab1c0e205908e5e3
snaveenandrew <snaveenandrew@gmail.com>
2017-03-20 21:40:47 +0530

    MediaControl Updated

--- Application/UserInterface/UserInterface/src/application/MediaControl.java
+++ Application/UserInterface/UserInterface/src/application/MediaControl.java
@@ -1,8 +1,10 @@
 package application;
 
 
-import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 import javafx.application.Platform;
 import javafx.beans.InvalidationListener;
@@ -12,25 +14,39 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaPlayer.Status;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.media.MediaView;
 import javafx.util.Duration;
 
 public class MediaControl extends BorderPane {
-
+	
+	String filename;
+	ImageView imageView;
+	Image image;
+	int x=0,y=0,w,h;
+	Group group = new Group();
 	Measure measure = new Measure();
 	
     private MediaPlayer mp;
@@ -45,15 +61,12 @@ public class MediaControl extends BorderPane {
     private HBox mediaBar;
 
     public MediaControl(final MediaPlayer mp) {
-    	    	
-        this.mp = mp;
-    	setStyle("-fx-padding: 10 10 10 10;  -fx-background-color:    linear-gradient(#686868 0%, #232723 25%, #373837 75%, #757575 100%),    linear-gradient(#020b02, #3a3a3a),    linear-gradient(#9d9e9d 0%, #6b6a6b 20%, #343534 80%, #242424 100%),    linear-gradient(#8a8a8a 0%, #6b6a6b 20%, #343534 80%, #262626 100%),    linear-gradient(#777777 0%, #606060 50%, #505250 51%, #2a2b2a 100%);    -fx-background-insets: 0,1,4,5,6;    -fx-background-radius: 9,8,5,4,3;");        
-        
+    	
+        this.mp = mp;       
     	mediaView = new MediaView(mp);
         mediaView.setFitWidth(measure.panelWidth); 
         mediaView.setFitHeight(measure.panelHeight); 
         mediaView.setPreserveRatio(false);
-       
         GridPane mvPane = new GridPane(); 
         mvPane.setAlignment(Pos.CENTER); 
         mvPane.setHgap(10); 
@@ -61,14 +74,13 @@ public class MediaControl extends BorderPane {
         mvPane.getChildren().add(mediaView);
         mvPane.setStyle("-fx-background-color: black;");
         mvPane.setAlignment(Pos.CENTER);
-        setCenter(mvPane);
-
         mediaBar = new HBox();
         mediaBar.setId("mediaBar");
         mediaBar.setAlignment(Pos.CENTER);
         mediaBar.setPadding(new Insets(5, 10, 5, 10));
         BorderPane.setAlignment(mediaBar, Pos.CENTER);
-        	
+        Label space = new Label("        ");
+        setCenter(space);
         //Play / Pause Button
         final Button playButton = new Button();
         playButton.setGraphic(setIcon("plugins\\icons\\playBlack.png"));
@@ -127,14 +139,14 @@ public class MediaControl extends BorderPane {
         		 WritableImage image = mediaView.snapshot(new SnapshotParameters(), null);
 
         		    // TODO: probably use a file chooser here
-        		    //File file = new File("chart.png");
-
-        		    //ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
-					BufferedImage bImage= SwingFXUtils.fromFXImage(image, null);
-					Image Nimage = SwingFXUtils.toFXImage(bImage, null);
-					ImagePreview preview = new ImagePreview(Nimage);
-					
-					//System.out.println(Nimage);
+        		    File file = new File("plugins\\capture\\capture.png");
+        		    try {
+						ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+        		    imageView.setImage(new Image("file:"+file));
         	}
 		});
         mp.currentTimeProperty().addListener(new InvalidationListener() {
@@ -199,8 +211,109 @@ public class MediaControl extends BorderPane {
         mediaBar.getChildren().add(playTime);
         mediaBar.getChildren().add(volumeLabel);
         mediaBar.getChildren().add(volumeSlider);
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(mvPane,mediaBar);
+        vbox.setStyle("-fx-padding: 10 10 10 10;  -fx-background-color:    linear-gradient(#686868 0%, #232723 25%, #373837 75%, #757575 100%),    linear-gradient(#020b02, #3a3a3a),    linear-gradient(#9d9e9d 0%, #6b6a6b 20%, #343534 80%, #242424 100%),    linear-gradient(#8a8a8a 0%, #6b6a6b 20%, #343534 80%, #262626 100%),    linear-gradient(#777777 0%, #606060 50%, #505250 51%, #2a2b2a 100%);    -fx-background-insets: 0,1,4,5,6;    -fx-background-radius: 9,8,5,4,3;");
+        setLeft(vbox);
         
-        setBottom(mediaBar);
+        //Image Panel
+        imageView = new ImageView();
+		image = new Image("file:plugins\\capture\\black.png");
+        VBox v2 = new VBox();
+        w = (int) measure.panelWidth;
+		h = (int) measure.panelHeight;
+		v2.setStyle("-fx-padding: 10 10 10 10;  -fx-background-color:    linear-gradient(#686868 0%, #232723 25%, #373837 75%, #757575 100%),    linear-gradient(#020b02, #3a3a3a),    linear-gradient(#9d9e9d 0%, #6b6a6b 20%, #343534 80%, #242424 100%),    linear-gradient(#8a8a8a 0%, #6b6a6b 20%, #343534 80%, #262626 100%),    linear-gradient(#777777 0%, #606060 50%, #505250 51%, #2a2b2a 100%);    -fx-background-insets: 0,1,4,5,6;    -fx-background-radius: 9,8,5,4,3;");
+		Pane imageViewParent = new Pane();
+		Rectangle rectBound = new Rectangle();
+		rectBound.setFill(Color.TRANSPARENT);
+	    rectBound.setStroke(Color.GOLD);     
+	    
+	     HBox hbox =new HBox();
+	     
+	     group.getChildren().add(imageView);
+	     
+	     imageView.setImage(image);
+	     imageView.setFitWidth(measure.panelWidth);
+	     imageView.setFitHeight(measure.panelHeight);
+	     imageView.setPreserveRatio(true);
+	     imageView.setSmooth(true);
+	     imageView.setCache(true);
+	     imageView.setLayoutX(0.0);
+	     imageView.setLayoutY(0.0);
+	     
+	     hbox.getChildren().add(imageView);
+	     hbox.getChildren().add(group);
+	     
+	     imageViewParent.getChildren().add(hbox);
+	     
+	     imageView.addEventFilter(MouseEvent.ANY, new EventHandler<MouseEvent>() {
+	         @Override
+	            public void handle(MouseEvent event) {
+	
+	                if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+	                    if (rectBound.getParent() == null) {
+	                        rectBound.setWidth(0.0); 
+	                        rectBound.setHeight(0.0);
+	                        rectBound.setLayoutX(event.getX()); 
+	                        rectBound.setLayoutY(event.getY());
+	                        x = (int) rectBound.getLayoutX();
+		                	y = (int) rectBound.getLayoutY();
+		                	// setX or setY
+	                        group.getChildren().add(rectBound);
+	                    }
+	                } else if (event.getEventType() == MouseEvent.MOUSE_RELEASED) {
+	
+	                } else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+	                    rectBound.setWidth(event.getX() - rectBound.getLayoutX());
+	                    rectBound.setHeight(event.getY() - rectBound.getLayoutY());
+	                    w = (int) rectBound.getWidth();
+	                	h = (int) rectBound.getHeight();
+	                } else if (event.getEventType() == MouseEvent.MOUSE_CLICKED
+	                        && event.getButton() == MouseButton.SECONDARY) {
+	
+	                    if (rectBound.getParent() != null) {
+	                        group.getChildren().remove(rectBound);
+	                    }
+	                } else if (event.getEventType() == MouseEvent.MOUSE_CLICKED
+	                        && event.getButton() == MouseButton.PRIMARY && event.getClickCount() > 1) {
+	                    //////////////// i crop here //////////////
+	                    
+	                	x = (int) rectBound.getLayoutX();
+	                	y = (int) rectBound.getLayoutY();
+	                	w = (int) rectBound.getWidth();
+	                	h = (int) rectBound.getHeight();                }
+	            }
+	    });
+
+	     group = new Group(imageView, rectBound);
+	
+	     HBox option = new HBox();
+	     option.setAlignment(Pos.CENTER);
+	     
+	     TextField Name = new TextField ();
+	     
+	     Button save = new Button();
+	     save.setGraphic(setIcon("plugins\\icons\\saveBlack.png"));
+	     
+	     save.setOnAction(new EventHandler<ActionEvent>() {
+	            public void handle(ActionEvent e){
+	                WritableImage image = imageView.snapshot(new SnapshotParameters(), null);
+	                PixelReader reader = image.getPixelReader();
+	                WritableImage newImage = new WritableImage(reader, (int)(x), (int)(y), w, h);
+	                    // TODO: probably use a file chooser here
+	                filename = Name.getText().toString()+"_"+String.valueOf(x)+"_"+String.valueOf(y)+"_"+String.valueOf(w)+"_"+String.valueOf(h);
+	                    File file = new File("plugins\\image\\input\\"+filename+".png");
+	                    try {
+	                        ImageIO.write(SwingFXUtils.fromFXImage(newImage, null), "png", file);
+	                    } catch (IOException ec) {
+	                        // TODO: handle exception here
+	                    }
+	            }
+	        }); 
+	     option.getChildren().add(Name);
+	     option.getChildren().add(save);
+	     v2.getChildren().addAll(imageViewParent,group,option);
+        setRight(v2);
     }
 
     protected void updateValues() {
